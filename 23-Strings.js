3e8b13692791c1a38eeb4db3712df5a632348a3b
Jawadahmaad10 <jawadjavedali@gmail.com>
2025-01-26 01:02:57 +0500

    Strings

--- 23-Strings.js
+++ 23-Strings.js
@@ -37,8 +37,8 @@ capitalizeName('javascript development is the best');
 //PadStart
 
 const message = 'Go to gate 23!';
-console.log(message.padStart(25, '+'));
+console.log(message.padStart(25, '+').padEnd(30,'+'));
 console.log('Jawad'.padStart(23,'+'));
 
 
-//
+
