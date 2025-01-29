03ea032c5dc2850653c3eae377dd4ea6304eb18f
Jawadahmaad10 <jawadjavedali@gmail.com>
2025-01-29 22:00:21 +0500

    .

---
+++ 10-Closer-Look-At-Functions/06-Call-Back-Functions.js
@@ -0,0 +1,16 @@
+
+
+const oneWord = function(str){
+   return str.replace(/ /g, '').toLowerCase(); 
+}
+
+
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join();
+}
+
+
+const transformer = function(str , fn){
+    
+}
