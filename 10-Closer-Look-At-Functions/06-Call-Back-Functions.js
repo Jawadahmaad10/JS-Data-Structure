459b334e259f84369c9b561d83363d2608ae3c82
Jawadahmaad10 <jawadjavedali@gmail.com>
2025-01-30 12:26:08 +0500

    call back functions

--- 10-Closer-Look-At-Functions/06-Call-Back-Functions.js
+++ 10-Closer-Look-At-Functions/06-Call-Back-Functions.js
@@ -10,7 +10,27 @@ const upperFirstWord = function(str){
     return [first.toUpperCase(), ...others].join();
 }
 
-
+//Higher-Order function 
 const transformer = function(str , fn){
-    
+console.log(`Original string: ${str}`);
+console.log(`Transformed string: ${fn(str)}`);
+
+console.log(`Transformed by: ${fn.name}`);
 }
+
+
+transformer('Javascript is the best!' , upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+
+//JS uses callbacks all the time
+const high5 = function(){
+    console.log('Waving at you');
+}
+
+document.body.addEventListener('click',high5);
+['Jawad','ahmad','khan'].forEach(high5);
+
+
+
+
