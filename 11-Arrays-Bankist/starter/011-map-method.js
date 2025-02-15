6625bfcb63a04bc64f4ed98399e9b2cc391c6f99
Jawadahmaad10 <jawadjavedali@gmail.com>
2025-02-16 00:08:25 +0500

    map-method

---
+++ 11-Arrays-Bankist/starter/011-map-method.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+
+
+/// MAP method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+
+// const movementsUSD = movements.map(function(mov){
+//    return mov * euroToUsd;
+// });
+
+// by using arraow function
+ 
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+
+// console.log(movementsUSD);
+
+
+// Can also do it using simple loop  method
+
+
+// const movementsUSDfor = [];
+
+// for(const mov of movements)  {
+//     movements.push( mov * movementsUSD)
+// };
+
+// console.log(movementsUSDfor);
+
+
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
+
